9a3905e55e09d5f833804b38ac128a5fcd5cd589
Trevor Senior <trevor@tsenior.com>
2014-05-23 12:03:04 -0400

    update readme with quickstart

---
+++ examples/node/readme-example-simple.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+"use strict";
+
+var Snoocore = require('../../Snoocore');
+var reddit = new Snoocore({ userAgent: 'myApp v0.0.0' });
+
+// Example code for http://www.reddit.com/dev/api#GET_new
+// [/r/*subreddit*]/new
+
+// Brackets `[]` denote optional sections of the URL, we can
+// Leave out [/r/subreddit] to get new posts site wide
+reddit.new().done(function(results) {
+	console.log(results);
+});
+
+// Or specify a subreddit with the url parameter *subreddit*
+reddit.r.$subreddit.new({ $subreddit: 'netsec' }).done(function(r) {
+	console.log(r);
+});
