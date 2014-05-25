e188f713bd7658acd6f4d921ecb4b1d99f5bfd0c
Trevor Senior <trevor@tsenior.com>
2014-05-25 17:41:51 -0400

    more robust test server for oauth

--- test/server/testServer.js
+++ test/server/testServer.js
@@ -2,45 +2,74 @@
 "use strict";
 
 var when = require('when')
-, delay = require('when/delay')
 , url = require('url')
 , http = require('http');
 
 exports.waitForRequest = function() {
-	var defer = when.defer();
 
-	var server = http.createServer(function(req, res) {
+	return when.promise(function(resolve, reject) {
 
-		if (req.method !== 'GET') { return res.end(); }
+		var server = http.createServer()
+		, connections = []
+		, received = false;
 
-		var parsedUrl = url.parse(req.url, true);
-		if (parsedUrl.pathname !== '/') { return res.end(); }
+		function killConnection(connection) {
+			connection.end();
+			connection.destroy();
+		}
 
-		// automatically close the window after it loads
-		var html = [
-			"<html><head></head><body>",
-			"Just a moment...",
-			"<script>",
-			"setTimeout(function() {",
-			"	window.open('','_self').close();",
-			"}, 1000);",
-			"</script>",
-			"</body></html>"
-		].join('\n');
+		function killAllConnections() {
+			connections.forEach(killConnection);
+		}
 
-		res.end(html);
+		server.on('request', function(req, res) {
 
-		// ugh, http://stackoverflow.com/a/14782123/586621
-		req.connection.end();
-		req.connection.destroy();
+			connections.push(req.connection);
 
-		server.close(function() {
-			var params = url.parse(req.url, true).query;
-			return defer.resolve(params);
+			var parsedUrl = url.parse(req.url, true);
+
+			if (req.method !== 'GET') {
+				killConnection(req.connection);
+				return res.end();
+			}
+
+			// favicon
+			if (parsedUrl.pathname !== '/') {
+				killConnection(req.connection);
+				return res.end();
+			}
+
+			// automatically close the window after it loads
+			var html = [
+				"<html><head></head><body>",
+				"Just a moment...",
+				"<script>",
+				"setTimeout(function() {",
+				"	window.open('','_self').close();",
+				"}, 1000);",
+				"</script>",
+				"</body></html>"
+			].join('\n');
+
+			res.end(html);
+
+			// Don't accept any other requests for this server
+			server.close();
+
+			// Once all connections close, return the parameters
+			server.once('close', function() {
+				return resolve(parsedUrl.query);
+			});
+
+			console.log('killing all connections'); //!!!debug
+			killAllConnections();
 		});
+
+		server.on('clientError', function(exception) {
+			return reject(exception);
+		});
+
+		server.listen(3000, '127.0.0.1');
 	});
 
-	server.listen(3000, '127.0.0.1');
-
-	return defer.promise;
 };
