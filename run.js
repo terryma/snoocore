b07cff04572d8cd669c461c5e224c75bee2398b1
Trevor Senior <trevor@tsenior.com>
2015-01-11 16:14:56 -0500

    update the reddit api endpoints

--- run.js
+++ run.js
@@ -9,15 +9,15 @@ var spawn = require('child_process').spawn;
 var build = require('snooform');
 
 exports.installModules = function(done) {
-    fs.exists(path.join(__dirname, 'node_modules'), function(exists) {
-        if (!exists) {
-            return exec('npm install', { cwd: __dirname }, function(error, stdout, stderr) {
-                return done(error);
-            });
-        } else {
-            return done();
-        }
-    });
+  fs.exists(path.join(__dirname, 'node_modules'), function(exists) {
+    if (!exists) {
+      return exec('npm install', { cwd: __dirname }, function(error, stdout, stderr) {
+        return done(error);
+      });
+    } else {
+      return done();
+    }
+  });
 };
 
 exports.buildRedditApi = function(done) {
@@ -47,59 +47,59 @@ exports.buildStandalone = function(done) {
 };
 
 exports.karmaTests = function(done) {
-    var karma = spawn(
-        path.join(__dirname, 'node_modules', 'karma', 'bin', 'karma'),
-        [ 'start' ],
-        { cwd: __dirname, stdio: 'inherit' }
-    );
+  var karma = spawn(
+    path.join(__dirname, 'node_modules', 'karma', 'bin', 'karma'),
+    [ 'start' ],
+    { cwd: __dirname, stdio: 'inherit' }
+  );
 
-    karma.on('exit', function(code) {
-        return (code !== 0)
-            ? done(new Error('Karma tests failed to run'))
-            : done();
-    });
+  karma.on('exit', function(code) {
+    return (code !== 0)
+           ? done(new Error('Karma tests failed to run'))
+			   : done();
+  });
 };
 
 
 exports.mochaTests = function(done) {
-    var mocha = spawn(
-        path.join(__dirname, 'node_modules', '.bin', 'mocha'),
-        [ '-R', 'spec' ],
-        { cwd: __dirname, stdio: 'inherit' }
-    );
+  var mocha = spawn(
+    path.join(__dirname, 'node_modules', '.bin', 'mocha'),
+    [ '-R', 'spec' ],
+    { cwd: __dirname, stdio: 'inherit' }
+  );
 
-    mocha.on('exit', function(code) {
-        return (code !== 0)
-            ? done(new Error('Mocha tests failed to run'))
-            : done();
-    });
+  mocha.on('exit', function(code) {
+    return (code !== 0)
+           ? done(new Error('Mocha tests failed to run'))
+			   : done();
+  });
 };
 
 exports.runTests = function(done) {
-    exports.karmaTests(function(error) {
-        if (error) {
-            return done(error);
-        }
-        return exports.mochaTests(function(error) {
-            if (error) {
-                return done(error);
-            }
+  exports.karmaTests(function(error) {
+    if (error) {
+      return done(error);
+    }
+    return exports.mochaTests(function(error) {
+      if (error) {
+        return done(error);
+      }
 
-            return done();
-        });
+      return done();
     });
+  });
 };
 
 exports.all = function(done) {
-    return exports.installModules(function(error) {
-        if (error) { return done(error); }
-        return exports.buildStandalone(function(error) {
-            if (error) { return done(error); }
-            return exports.runTests(function(error) {
-                return error ? done(error) : done();
-            });
-        });
+  return exports.installModules(function(error) {
+    if (error) { return done(error); }
+    return exports.buildStandalone(function(error) {
+      if (error) { return done(error); }
+      return exports.runTests(function(error) {
+        return error ? done(error) : done();
+      });
     });
+  });
 };
 
 // ---
@@ -109,34 +109,34 @@ var argv = process.argv.slice(2);
 var fn;
 
 switch(argv[0]) {
-case 'all':
+  case 'all':
     fn = exports.all;
     break;
-case 'browserify':
-case 'standalone':
+  case 'browserify':
+  case 'standalone':
     fn = exports.buildStandalone;
     break;
-case 'test':
+  case 'test':
     fn = exports.runTests;
     break;
-case 'mocha':
+  case 'mocha':
     fn = exports.mochaTests;
     break;
-case 'karma':
+  case 'karma':
     fn = exports.karmaTests;
     break;
-case 'api':
+  case 'api':
     fn = exports.buildRedditApi;
     break;
-default:
+  default:
     fn = function(done) {
-        return done(new Error('invalid subcommand'));
+      return done(new Error('invalid subcommand'));
     };
 }
 
 fn(function(error) {
-    if (error) {
-        console.error(error.stack);
-        process.exit(1);
-    }
+  if (error) {
+    console.error(error.stack);
+    process.exit(1);
+  }
 });
