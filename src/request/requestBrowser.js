67f9e50f97fa2044818f89a0bbe8ce3673949e3f
Trevor Senior <trevor@tsenior.com>
2015-03-21 19:19:12 -0400

    whitespace adjustment

--- src/request/requestBrowser.js
+++ src/request/requestBrowser.js
@@ -25,23 +25,23 @@ exports.https = function(options, formData) {
 
       // append the form data to the end of the url
       if (options.method === 'GET') {
-	url += '?' + data.buffer.toString();
+        url += '?' + data.buffer.toString();
       }
 
       x.open(options.method, url, true);
 
       Object.keys(options.headers).forEach(function(headerKey) {
-	x.setRequestHeader(headerKey, options.headers[headerKey]);
+        x.setRequestHeader(headerKey, options.headers[headerKey]);
       });
 
       x.onreadystatechange = function() {
-	if (x.readyState > 3) {
-	  // Normalize the result to match how requestNode.js works
-	  return resolve({
-	    _body: x.responseText,
-	    _status: x.status
-	  });
-	}
+        if (x.readyState > 3) {
+          // Normalize the result to match how requestNode.js works
+          return resolve({
+            _body: x.responseText,
+            _status: x.status
+          });
+        }
       };
 
       x.send(options.method === 'GET' ? null : data.buffer.toString());
