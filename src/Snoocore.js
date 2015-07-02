e42544753bb4389960dce63557218a21dad008bf
Trevor Senior <trevor@tsenior.com>
2015-07-02 18:14:11 -0400

    3.1.0

--- src/Snoocore.js
+++ src/Snoocore.js
@@ -13,7 +13,7 @@ import fileHelper from './https/file';
 export default class Snoocore extends events.EventEmitter {
 
   static get version() {
-    return '3.0.2';
+    return '3.1.0';
   }
 
   static file(...args) {
